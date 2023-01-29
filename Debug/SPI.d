0b3838c948ae304f93c18391865c5236e45d9fb3
CIRA2606 <78839818+CIRA2606@users.noreply.github.com>
2023-01-29 03:34:58 -0600

    Termopar
    Práctica 7 SPI, usando termopar y encendedor

---
+++ Debug/SPI.d
@@ -0,0 +1,87 @@
+# FIXED
+
+SPI.obj: ../SPI.c
+SPI.obj: ../include.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdlib.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/string.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/xlocale/_string.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/math.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_defs.h
+SPI.obj: D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_limits.h
+SPI.obj: ../SPI.h
+SPI.obj: ../TM4C123GH6PM.h
+SPI.obj: ../core_cm4.h
+SPI.obj: ../core_cmInstr.h
+SPI.obj: ../cmsis_ccs.h
+SPI.obj: ../core_cmFunc.h
+SPI.obj: ../core_cm4_simd.h
+SPI.obj: ../system_TM4C123.h
+SPI.obj: ../PLL.h
+
+../SPI.c:
+
+../include.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdlib.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/string.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/xlocale/_string.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/math.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_defs.h:
+
+D:/PROGRAMAS/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_limits.h:
+
+../SPI.h:
+
+../TM4C123GH6PM.h:
+
+../core_cm4.h:
+
+../core_cmInstr.h:
+
+../cmsis_ccs.h:
+
+../core_cmFunc.h:
+
+../core_cm4_simd.h:
+
+../system_TM4C123.h:
+
+../PLL.h:
+
